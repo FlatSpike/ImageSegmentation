69aabc31d862bcc9234b3ccf23df20992ee36e1b
Bashurin <Bashkinator@gmail.com>
2014-12-17 16:17:24 +0300

    Create distance functions

--- Clustering/Clustering.cs
+++ Clustering/Clustering.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
 
 namespace Clustering
 {
-    public class Clustering
+    public static class Clustering
     {
+        public static List<Cluster> MeanShiftClustering(List<Point3D> points, Func<Point3D, Point3D, double> criteria)
+        {
+            // TODO: Implement this
+            return null;
+        }
+
+        // TODO: Implement k-means algorithm
+         
     }
 }
---
+++ Clustering/Criteria.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Media.Media3D;
+
+namespace Clustering
+{
+    public static class Criteria
+    {
+        public static double EuclideanDistance(Point3D a, Point3D b)
+        {
+            return (a - b).Length;
+        }
+
+        public static double EuclideanDistanceSquared(Point3D a, Point3D b)
+        {
+            return (a - b).LengthSquared;
+        }
+
+        // Also known as Manhattan Distance
+        public static double TaxicabDistance(Point3D a, Point3D b)
+        {
+            Vector3D v = a - b;
+            return Math.Abs(v.X) + Math.Abs(v.Y) + Math.Abs(v.Z);
+        }
+
+        // Also known as Chessboard Distance
+        public static double ChebyshevDistance(Point3D a, Point3D b)
+        {
+            Vector3D v = a - b;
+            return (new double[] {v.X, v.Y, v.Z}).Max();
+        }
+    }
+}
